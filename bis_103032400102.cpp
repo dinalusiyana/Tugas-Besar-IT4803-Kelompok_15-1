33f1f76e20f6636b51a096327e8f9eb8b253e59d
dinalusiyana <dinalusiyana09@gmail.com>
2025-11-30 21:33:08 +0700

    bis_103032400102.cpp

--- bis_103032400102.cpp
+++ bis_103032400102.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+#include "bis.h"
 
+using namespace std;
+
+void deleteFirst(List &L, address &p){
+    if (L.first == nullptr) {
+        p = nullptr;
+    } else {
+        p = L.first;
+        L.first = p->next;
+        p->next = nullptr;
+    }
+}
+
+void deleteLast(List &L, address &p){
+    if (L.first == nullptr) {
+        p = nullptr;
+    } else if (L.first->next == nullptr){
+        p = L.first;
+        L.first = nullptr;
+    } else {
+        address Q = L.first;
+        while (Q->next->next != nullptr){
+            Q = Q->next;
+        }
+        P = Q->next;
+        Q->next = nullptr;
+    }
+}
+
+void deleteAfter(List &L, address prec, address &p){
+    if (prec == nullptr || prec->next == nullptr) {
+        p = nullptr;
+    } else {
+        p = prec->next;
+        prec->next = p->next;
+        p->next = nullptr;
+    }
+}
+
+address findBus(List L, string namaBus){
+    address p = L.first;
+    while (p != nullptr) {
+        if (p->info.namaBus == namaBus){
+            return P;
+        }
+        p = p->next;
+    }
+    return nullptr;
+}
+
+address showAll(List L){
+    return L.first;
+}
